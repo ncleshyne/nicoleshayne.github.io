2ff111ebd5832e59741473b07a6f00e1d697d792
unknown <ncleshyne@gmail.com>
2020-08-07 18:13:08 +0800

    Added a new section

--- js/script.js
+++ js/script.js
@@ -7,9 +7,21 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
 
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1500);
-    return false;
+$('a').click(function(){
+  $('html, body').animate({
+      scrollTop: $( $(this).attr('href') ).offset().top
+  }, 1500);
+  return false;
 });
+
+var navbar = document.querySelector('nav')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
